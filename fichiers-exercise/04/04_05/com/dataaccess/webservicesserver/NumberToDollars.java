30f96719209f068958114d8510da61626cc92989
LinkedIn User <linkedin@MGR2-BTH109M.linkedin.biz>
2023-11-08 15:22:43 +0100

    04_05_d

---
+++ fichiers-exercise/04/04_05/com/dataaccess/webservicesserver/NumberToDollars.java
@@ -0,0 +1,65 @@
+
+package com.dataaccess.webservicesserver;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="dNum" type="{http://www.w3.org/2001/XMLSchema}decimal"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "dNum"
+})
+@XmlRootElement(name = "NumberToDollars")
+public class NumberToDollars {
+
+    @XmlElement(required = true)
+    protected BigDecimal dNum;
+
+    /**
+     * Obtient la valeur de la propriété dNum.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getDNum() {
+        return dNum;
+    }
+
+    /**
+     * Définit la valeur de la propriété dNum.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setDNum(BigDecimal value) {
+        this.dNum = value;
+    }
+
+}
